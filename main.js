ff51ec74f3ad5f69fc16236488e5fc651d938961
joydeep808 <113516173+joydeep808@users.noreply.github.com>
2023-04-11 12:36:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+const mobile_nav_btn = document.querySelector('.temp')
+const  nav_bar = document.querySelector('.nav-bar')
+mobile_nav_btn.addEventListener('click',()=>{
+    nav_bar.classList.toggle('nav-open')
+
+})
+
+
+let name= new Typed(".name",{
+    strings:[" Joydeep "],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+new Typed(".instror",{
+    strings:[" Joydeep "],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+let w =66
+console.log(w)
